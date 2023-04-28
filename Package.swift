1ccb593eba3b1119be2f8c829236f82c7e1bb205
Deniz Eroglu <denizeroglu@me.com>
2023-04-28 16:30:35 +0200

    Clean up

--- Package.swift
+++ Package.swift
@@ -10,13 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .plugin(name: "TiseFormatterPlugin", targets: ["TiseFormatterPlugin"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .plugin(
             name: "TiseFormatterPlugin",
             capability: .buildTool(),
--- Plugins/TiseFormatterPlugin/TiseFormatterPlugin.swift
+++ Plugins/TiseFormatterPlugin/TiseFormatterPlugin.swift
@@ -4,9 +4,11 @@ import PackagePlugin
 @main
 public struct TiseFormatterPlugin: BuildToolPlugin {
 
-    public init() {
-        print("formatter init")
-    }
+    // MARK: Lifecycle
+
+    public init() {}
+
+    // MARK: Public
 
     public func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         guard let sourceTarget = target as? SourceModuleTarget else {
@@ -24,9 +26,11 @@ public struct TiseFormatterPlugin: BuildToolPlugin {
                 packageDirectory: context.package.directory,
                 workingDirectory: context.pluginWorkDirectory,
                 tool: try context.tool(named: "swiftformat")
-            ),
+            )
         ].compactMap { $0 }
     }
+
+    // MARK: Private
 
     private func format(
         inputFiles: [Path],
@@ -36,27 +40,14 @@ public struct TiseFormatterPlugin: BuildToolPlugin {
     ) -> Command? {
 
         if inputFiles.isEmpty {
-            // Don't lint anything if there are no Swift source files in this target
             return nil
         }
 
         var arguments: [String] = [
-//            "lint",
-//            "--quiet",
-//            // We always pass all of the Swift source files in the target to the tool,
-//            // so we need to ensure that any exclusion rules in the configuration are
-//            // respected.
-//            "--force-exclude",
-//            "--cache-path", "\(workingDirectory)"
+            "--config", "\(packageDirectory.string + "/FormatRules/tise.swiftformat")"
         ]
 
-        // Manually look for configuration files, to avoid issues when the plugin does not execute our tool from the
-        // package source directory.
-
-        arguments.append(contentsOf: ["--config", "\(packageDirectory.string + "/FormatRules/tise.swiftformat")"])
         arguments += inputFiles.map(\.string)
-
-        // We are not producing output files and this is needed only to not include cache files into bundle
         let outputFilesDirectory = workingDirectory.appending("Output")
 
         return
@@ -67,8 +58,6 @@ public struct TiseFormatterPlugin: BuildToolPlugin {
                 outputFilesDirectory: outputFilesDirectory
             )
     }
-
-
 
     private func lint(
         inputFiles: [Path],
@@ -101,8 +90,6 @@ public struct TiseFormatterPlugin: BuildToolPlugin {
         // We are not producing output files and this is needed only to not include cache files into bundle
         let outputFilesDirectory = workingDirectory.appending("Output")
 
-        print("Tool", tool)
-
         return
             .prebuildCommand(
                 displayName: "SwiftLint",
@@ -118,14 +105,10 @@ import XcodeProjectPlugin
 
 extension TiseFormatterPlugin: XcodeBuildToolPlugin {
     public func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
-        print("XcodeProjectPlugin init")
-        print("🥊", context)
 
         let inputFilePaths = target.inputFiles
             .filter { $0.type == .source && $0.path.extension == "swift" }
             .map(\.path)
-
-        print("🥊", target)
 
         return [
             lint(
@@ -144,41 +127,3 @@ extension TiseFormatterPlugin: XcodeBuildToolPlugin {
     }
 }
 #endif
-
-
-import Foundation
-import PackagePlugin
-
-extension Path {
-    /// Scans the receiver, then all of its parents looking for a configuration file with the name ".swiftlint.yml".
-    ///
-    /// - returns: Path to the configuration file, or nil if one cannot be found.
-    func firstConfigurationFileInParentDirectories() -> Path? {
-        let defaultConfigurationFileName = ".swiftlint.yml"
-        let proposedDirectory = sequence(
-            first: self,
-            next: { path in
-                guard path.stem.count > 1 else {
-                    // Check we're not at the root of this filesystem, as `removingLastComponent()`
-                    // will continually return the root from itself.
-                    return nil
-                }
-
-                return path.removingLastComponent()
-            }
-        ).first { path in
-            let potentialConfigurationFile = path.appending(subpath: defaultConfigurationFileName)
-            return potentialConfigurationFile.isAccessible()
-        }
-        return proposedDirectory?.appending(subpath: defaultConfigurationFileName)
-    }
-
-    /// Safe way to check if the file is accessible from within the current process sandbox.
-    private func isAccessible() -> Bool {
-        let result = string.withCString { pointer in
-            access(pointer, R_OK)
-        }
-
-        return result == 0
-    }
-}
